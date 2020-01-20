f4dd8524dac9fcdf3ea2809c5d5546a218a28da4
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-01-21 01:40:20 +0200

    Refactor to class

---
+++ JavaScript/3-class.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const TIME_STEP = 500;
+
+class StateMachine {
+  constructor() {
+    this.state = StateMachine.STATE.INIT;
+    this.timer = setInterval(() => {
+      switch (this.state) {
+      case StateMachine.STATE.INIT:
+        console.log('initialization');
+        this.state = StateMachine.STATE.WORK;
+        break;
+      case StateMachine.STATE.WORK:
+        process.stdout.write('.');
+        break;
+      case StateMachine.STATE.FIN:
+        console.log('\nfinalization');
+        this.state = StateMachine.STATE.EXIT;
+        break;
+      case StateMachine.STATE.EXIT:
+        console.log('exit');
+        clearInterval(this.timer);
+      }
+    }, TIME_STEP);
+  }
+}
+
+StateMachine.STATE = {
+  INIT: 0,
+  WORK: 1,
+  FIN: 2,
+  EXIT: 3,
+};
+
+const sm = new StateMachine();
+process.on('SIGINT', () => {
+  sm.state = StateMachine.STATE.FIN;
+});
