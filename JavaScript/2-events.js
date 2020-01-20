aaef7ecc5a10f16642104108106c6f67cb5b4acb
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-01-20 23:55:16 +0200

    Add event-based implementation

---
+++ JavaScript/2-events.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const TIME_STEP = 500;
+
+const STATE_INIT = 0;
+const STATE_WORK = 1;
+const STATE_FIN = 2;
+const STATE_EXIT = 3;
+
+let state = STATE_INIT;
+let timer;
+
+process.on('SIGINT', () => state = STATE_FIN);
+
+const step = () => {
+  switch (state) {
+  case STATE_INIT:
+    console.log('initialization');
+    state = STATE_WORK;
+    break;
+  case STATE_WORK:
+    process.stdout.write('.');
+    break;
+  case STATE_FIN:
+    console.log('\nfinalization');
+    state = STATE_EXIT;
+    break;
+  case STATE_EXIT:
+    console.log('exit');
+    clearInterval(timer);
+  }
+};
+
+timer = setInterval(step, TIME_STEP);
